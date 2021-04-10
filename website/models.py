16c94f12e2423fe21c03d4f42c8cdec695948a11
barel31 <barel123133@gmail.com>
2021-04-10 13:36:02 +0300

    Add files via upload

---
+++ website/models.py
@@ -0,0 +1,13 @@
+from . import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users' # old database table name
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    score = db.Column(db.Integer)
+
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
