3734310c123372aa679732f6657e39a2d485b2c2
barel31 <barel123133@gmail.com>
2022-02-12 15:14:45 +0200

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
 
     app.secret_key = "barel31"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zzeoiakyeudtph:6d662bd9771ce97f738b769f29d3100ca8996d41c26bbc3cbec6d4742cd904a2@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d5e7lpuv8v66ck'#f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zzeoiakyeudtph:6d662bd9771ce97f738b769f29d3100ca8996d41c26bbc3cbec6d4742cd904a2@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d5e7lpuv8v66ck'#f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
