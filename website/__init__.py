d4001b40698c441bdc254501431115c94e74a6db
barel31 <barel123133@gmail.com>
2022-05-17 19:05:22 +0300

    Added variables to hold db seceret key and uri

--- website/__init__.py
+++ website/__init__.py
@@ -5,13 +5,14 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = 'test2.sqlite3'
-
+DB_SECRET = 'barel31'
+DB_URI = 'postgresql://zzeoiakyeudtph:6d662bd9771ce97f738b769f29d3100ca8996d41c26bbc3cbec6d4742cd904a2@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d5e7lpuv8v66ck'#f'sqlite:///{DB_NAME}'
 
 def create_app():
     app = Flask(__name__)
 
-    app.secret_key = "barel31"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zzeoiakyeudtph:6d662bd9771ce97f738b769f29d3100ca8996d41c26bbc3cbec6d4742cd904a2@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d5e7lpuv8v66ck'#f'sqlite:///{DB_NAME}'
+    app.secret_key = DB_SECRET
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
